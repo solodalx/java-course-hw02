f9d1a081eb174a0acdb2d3ca6d259d9f1d555aae
solodalx <solodukha@mail.ru>
2017-10-29 00:01:30 +0300

    v1.2

--- src/sgu/Solution.java
+++ src/sgu/Solution.java
@@ -1,9 +1,7 @@
 package sgu;
 
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 import static java.util.stream.Stream.of;
 
@@ -14,16 +12,32 @@ public class Solution {
         Scanner scanner = new Scanner(System.in);
         PrintWriter writer = new PrintWriter(System.out);
 
-        String input = scanner.nextLine();
+        String input = "", line = null;
+        while ((line = scanner.nextLine()).length() > 0) {
+            input += line + "\n";
+        }
         String delims = "[ \n]+";
         String[] words = input.split(delims);
 
-        Map<String, Integer> wordCount = new HashMap<String, Integer>();
+        Map<String, Integer> wordCount = new TreeMap<>();
         for (String word : words) {
-            int count = wordCount.get(word);
+            word = word.toLowerCase();
+            Integer count = wordCount.get(word);
+            if (count == null) {
+                count = 0;
+            }
             wordCount.put(word, count + 1);
+//            System.out.println(word + ": " + (count + 1));
         }
 
+        Integer max = Collections.max(wordCount.entrySet(), (entry1, entry2) -> entry1.getValue() - entry2.getValue()).getValue();
+//        System.out.println(max);
+
+        for (Map.Entry<String, Integer> set : wordCount.entrySet()) {
+            if (set.getValue() == max) {
+                System.out.println(set.getKey());
+            }
+        }
 
     }
 
